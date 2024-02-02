f8ffd7f597cfe503398c2a21256ad1bcf93fbe3a
auqid <38567390+auqid@users.noreply.github.com>
2024-02-02 19:49:35 +0530

    Added a navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    
+        <nav className='navbar'>
+            <a href="/" className='nav-1'>Auqid Irfan</a>
+                <a href="/" className='nav-2'>Home</a>
+                <a href="/contact" className='nav-2'>Contact</a>
+                <a href="/about" className='nav-2'>About</a>
+                <a href="/portfolio" className='nav-2'>Portfolio</a>
+      </nav>
+  )
+}
+
+export default Header
