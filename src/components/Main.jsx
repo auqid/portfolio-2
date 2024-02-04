4673c33ebe3f9862af4c19303e1b3c2a67597ae2
auqid <38567390+auqid@users.noreply.github.com>
2024-02-04 17:55:39 +0530

    added typewritter effect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './main.css'
+import { ReactTyped } from 'react-typed'
 import cube from '../assets/png-transparent-3-by-3-rubik-s-cube-art-t-shirt-rubiks-cube-drawing-color-hand-painted-cube-watercolor-painting-color-splash-hand-thumbnail.png'
 
 const Main = () => {
   return (
    <div className='container-main'>
    <p className='container-text'>
-   <span className='container-text1'>Fullstack & Frontend Developer.</span><br/>
+    <div className='container-text1'>
+     <span>I am a </span>
+    <ReactTyped
+      strings={[
+        " Full Stack Developer.",
+        " Front-End Developer.",
+        " Back-End Developer.",
+      ]}
+      typeSpeed={50}
+      backSpeed={70}
+      loop
+      />
+    
+    </div><br/>
   I design and code beautifully simple things, and I love what I do.
    </p>
    <img src={cube} alt='cube' className='container-img'/>
