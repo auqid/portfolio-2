eb41ec893f9c45acb57b22da848004a12e8f4bdc
auqid <38567390+auqid@users.noreply.github.com>
2024-02-09 14:17:58 +0530

    changes to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Skills.css'
-import code from '../assets/Code.svg..png'
+//import code from '../assets/Code.svg..png'
+import cal from '../assets/icon-calculator.svg'
+import kar from '../assets/icon-karma.svg'
+import sup from '../assets/icon-supervisor.svg'
+import bul from '../assets/icon-team-builder.svg'
+
+
 
 const Skills = () => {
   return (
     <>
-<div className='container-skills'>
+{/* <div classNameName='container-skills'>
 
-  <div className='container-item'>
-  <img className='container-skills-img'src={code} alt='code logo'/>
+  <div classNameName='container-item'>
+  <img classNameName='container-skills-img'src={code} alt='code logo'/>
   <h1>Full Stack Developer</h1>
   <h3>I like to code things from scratch, and <br/>enjoy  bringing ideas to life in the browser.</h3>
   <h4>Languages I speak:</h4>
@@ -20,7 +26,45 @@ const Skills = () => {
   <h4>Database</h4>
   <h3>MySQL, MongoDB.</h3>
   </div>
-   </div>
+   </div> */}
+
+
+<section className="cards">
+  <div className="card-team-builder">
+      <h1 className="card-title">Full Stack Developer</h1>
+      <p className="card-paragraph">I like to code things from scratch, and enjoy  bringing ideas to life in the browser.</p>
+      <div className="card-image">
+        <img src={bul} alt='builder-logo'/>
+      </div>
+    </div>
+   
+   
+    <div className="card-supervisor">
+      <h1 className="card-title">Languages I speak:</h1>
+      <p className="card-paragraph">JavaScript, Python.</p>
+      <div className="card-image">
+        <img src={sup} alt='supervisor-logo'/>
+      </div>
+    </div>
+
+    
+    
+    <div className="card-karma">
+      <h1 className="card-title">Front End</h1>
+      <p className="card-paragraph">ReactJS, HTML, CSS</p>
+      <div className="card-image">
+        <img src={kar} alt='karma-logo'/>
+      </div>
+    </div>
+
+    <div className="card-calculator">
+      <h1 className="card-title">Backend</h1>
+      <p className="card-paragraph">Node.js, Express.</p>
+      <div className="card-image">
+        <img src={cal} alt='calculator-img'/>
+      </div>
+    </div>
+  </section>
 
    </>
   )
