7a2803142a97a76cbf9a1295fcea95118a38bbf6
auqid <auqidirfan100@gmail.com>
2025-01-22 17:53:41 +0530

    added more projects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,52 +1,88 @@
-import React from 'react'
-import './contact.css'
-import {FaLinkedin, FaGithub} from 'react-icons/fa'
+import React from "react";
+import "./contact.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div>
-      <section  id='contact' className="contact">
-  <div className="contact-header">
-    <h3 id="co-title">Contact Me</h3>
-    <h1 id="co-text">Reach out for a new project or to say hello.</h1>
-  </div>
-  
-  <div className="contact-content">
-    <div className="contact-form">
-      <h3 id="form-title">Send me a message</h3>
-    <form action="mailto:auqidirfan100@gmail.com" method="post" enctype="text/plain">
-      <fieldset>
-        <div className="form-field">
-          <input name="name" type="text" id="name" placeholder="Your Name"></input>
+      <section id="contact" className="contact">
+        <div className="contact-header">
+          <h3 id="co-title">Contact Me</h3>
+          <h1 id="co-text">Reach out for a new project or to say hello.</h1>
         </div>
-      <div className="form-field">
-          <input name="email" type="email" id="email" placeholder="Your Email"></input>
-        </div>
-      <div className="form-field">
-          <input name="Subject" type="text" id="subject" placeholder="Subject"></input>
-        </div>
-    <div className="form-field">
-          <textarea name="message" type="text" id="message" placeholder="Your Message"></textarea>
-        </div>
-      </fieldset>
-  <input id="form-btn" type="submit" value="send"/>
-    </form>
-</div>
-  
-    <div className="contact-info">
-      <h3>Email Me At </h3>
-      <a href="mailto:auqidirfan100@gmail.com"><p>auqidirfan100@gmail.com</p></a>
-      <h3>Find Me On </h3>
-      <div className="contact-links">
-        <a href='/https://www.linkedin.com/in/auqidirfan/'><i className="fab fa-linkedin"><FaLinkedin/></i></a>
-        <a href='/https://www.github.com/auqid'><i className="fab fa-github"><FaGithub/></i></a>
-        <a href='/'><i className="fab fa-codepen"></i></a>
-      </div>
-    </div>
-    </div>
-</section>
-    </div>
-  )
-}
 
-export default Contact
+        <div className="contact-content">
+          <div className="contact-form">
+            <h3 id="form-title">Send me a message</h3>
+            <form
+              action="mailto:auqidirfan100@gmail.com"
+              method="post"
+              enctype="text/plain"
+            >
+              <fieldset>
+                <div className="form-field">
+                  <input
+                    name="name"
+                    type="text"
+                    id="name"
+                    placeholder="Your Name"
+                  ></input>
+                </div>
+                <div className="form-field">
+                  <input
+                    name="email"
+                    type="email"
+                    id="email"
+                    placeholder="Your Email"
+                  ></input>
+                </div>
+                <div className="form-field">
+                  <input
+                    name="Subject"
+                    type="text"
+                    id="subject"
+                    placeholder="Subject"
+                  ></input>
+                </div>
+                <div className="form-field">
+                  <textarea
+                    name="message"
+                    type="text"
+                    id="message"
+                    placeholder="Your Message"
+                  ></textarea>
+                </div>
+              </fieldset>
+              <input id="form-btn" type="submit" value="send" />
+            </form>
+          </div>
+
+          <div className="contact-info">
+            <h3>Email Me At </h3>
+            <a href="mailto:auqidirfan100@gmail.com">
+              <p>auqidirfan100@gmail.com</p>
+            </a>
+            <h3>Find Me On </h3>
+            <div className="contact-links">
+              <a href="/https://www.linkedin.com/in/auqidirfan/">
+                <i className="fab fa-linkedin">
+                  <FaLinkedin />
+                </i>
+              </a>
+              <a href="/https://www.github.com/auqid">
+                <i className="fab fa-github">
+                  <FaGithub />
+                </i>
+              </a>
+              <a href="/">
+                <i className="fab fa-codepen"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,68 +1,90 @@
-import React from 'react'
-import IMG1 from '../assets/portfolio1.jpg'
-import IMG2 from '../assets/portfolio2.jpg'
-import IMG3 from '../assets/portfolio3.jpg'
-import './projects.css'
+import React from "react";
+import IMG1 from "../assets/portfolio1.jpg";
+import IMG4 from "../assets/miles.png";
+import IMG5 from "../assets/viva.png";
+import IMG6 from "../assets/pop.png";
+import IMG7 from "../assets/like.png";
+import IMG8 from "../assets/hapi.png";
+import IMG9 from "../assets/uk.png";
+import "./projects.css";
 
-const data=[
-  {id:1,
-   image: IMG1,
-   title: 'A-Z Shop Using MERN Stack.',
-   github: 'https://github.com/auqid/azshop',
-   demo: 'https://azshop.onrender.com/'
-
+const data = [
+  {
+    id: 2,
+    image: IMG4,
+    title: "Milespacios",
+    github: " ",
+    demo: "https://milespacios.com/",
   },
-
-  {id:2,
-   image: IMG2,
-   title: 'Weather App using NodeJS.',
-   github: 'https://github.com/auqid/weather-app',
-   demo: 'https://github.com/auqid/weather-app'
-  
-    },
-  {id:3,
-   image: IMG3,
-   title: 'TO-DO list using React Rick and Morty Theme.',
-   github: 'https://github.com/auqid/todo-list-react',
-   demo: 'https://github.com/auqid/todo-list-react',
-  }
-
-
-
-]
-
-
+  {
+    id: 3,
+    image: IMG5,
+    title: "Vivacity",
+    github: " ",
+    demo: "https://vivacity.studio",
+  },
+  {
+    id: 4,
+    image: IMG6,
+    title: "Pop Nouvelle",
+    github: "",
+    demo: "https://popnouvelle.com/",
+  },
+  {
+    id: 1,
+    image: IMG1,
+    title: "A-Z Shop Using MERN Stack.",
+    github: "https://github.com/auqid/azshop",
+    demo: "https://azshop.onrender.com/",
+  },
+  {
+    id: 5,
+    image: IMG7,
+    title: "LikeNewOnly",
+    github: "",
+    demo: "https://likenewonly-dev-788d4e524eac.herokuapp.com/",
+  },
+  {
+    id: 6,
+    image: IMG8,
+    title: "Hapiklan",
+    github: "",
+    demo: "https://hapiklan.com/",
+  },
+  {
+    id: 6,
+    image: IMG9,
+    title: "Ukreate",
+    github: "",
+    demo: "https://ukreate.com/",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
-      
-      
+    <section id="portfolio">
       <div className="container portfolio__container">
-      
-        {
-          data.map(({id,image,title,github,demo}) =>{
-            return(
-              <article key={id} className='portfolio_item'>
-                <div className='portfolio'>
-                  <img  className='portfolio-img' src={image} alt={title}/>
-                </div>
-                <h3>{title}</h3>
-                <div className='portfolio__item-cta'>
-                  <a href ={github} className='btn' target='blank'>Github</a>
-                  <a href ={demo} className='btn btn-primary' target='blank'>Live Demo</a>
-
-                </div>
-              </article>
-
-
-
-            )
-          })
-        }
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio">
+                <img className="portfolio-img" src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                {/* <a href={github} className="btn" target="blank">
+                  Github
+                </a> */}
+                <a href={demo} className="btn btn-primary" target="blank">
+                  Check it out!
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
