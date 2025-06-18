9693af96f448062753411c2731cd355f726b0ba7
auqid <auqidirfan100@gmail.com>
2025-06-18 12:13:30 +0530

    minor fixes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,47 +1,44 @@
-import React from 'react'
-import './main.css'
-import { ReactTyped } from 'react-typed'
-import { Suspense} from 'react'
-import {Canvas} from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
-import Cube from './Cube'
+import React from "react";
+import "./main.css";
+import { ReactTyped } from "react-typed";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import Cube from "./Cube";
 
 const Main = () => {
   return (
-   <div className='container-main'>
-   <p className='container-text'>
-    <div className='container-text1'>
-     <span>I am a </span>
-    <ReactTyped
-      strings={[
-        " Full Stack Developer.",
-        " Front-End Developer.",
-        " Back-End Developer.",
-      ]}
-      typeSpeed={50}
-      backSpeed={70}
-      loop
-      />
-    
-    </div><br/>
-  I design and code beautifully simple things, and I love what I do.
-   </p>
-   {/* <img src={cube} alt='cube' className='container-img'/> */}
-   <Canvas>
-    <ambientLight intensity={1.5}/>
-    <OrbitControls enableZoom={false}/>
-    <Suspense fallback={null}>
-      <Cube/>
-    </Suspense>
-    <Environment preset='sunset'/>
-   </Canvas>
-   
-  
-   
-   
-   
-   </div>
-  )
-}
+    <div className="container-main">
+      <p className="container-text">
+        <div className="container-text1">
+          <span>I am a </span>
+          <ReactTyped
+            strings={[
+              " Full Stack Developer.",
+              " Front-End Developer.",
+              " Back-End Developer.",
+            ]}
+            typeSpeed={50}
+            backSpeed={70}
+            loop
+          />
+        </div>
+        <br />
+      </p>{" "}
+      <div className="container-text2">
+        I design and code beautifully simple things, and I love what I do.
+      </div>
+      {/* <img src={cube} alt='cube' className='container-img'/> */}
+      <Canvas>
+        <ambientLight intensity={1.5} />
+        <OrbitControls enableZoom={false} />
+        <Suspense fallback={null}>
+          <Cube />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
